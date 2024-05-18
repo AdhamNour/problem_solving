4abcccaa53dc3d607ff21f6a60058d9fc6126bfe
AdhamNour <adhamnourelwaffaa@gmail.com>
2024-05-18 22:42:49 +0300

    solved the problem in C

--- best-time-to-sell-and-buy-stocks/main.c
+++ best-time-to-sell-and-buy-stocks/main.c
@@ -5,11 +5,18 @@ int maxProfit(int* prices, int pricesSize) {
 	int smallestNumber = 10*10*10*10; // smallest number found in the array;  chosen to be thw maximum number possible for.the.input
 	int i=0; // loop index
 	for(i=0;i<pricesSize;i++){
-
+		if(prices[i]<smallestNumber){
+			smallestNumber =prices[i];
+		}
+		else if (prices[i]-smallestNumber>maximumProfit){
+			maximumProfit = prices[i]-smallestNumber;
+		}
 	}
+	return maximumProfit;
 }
 
 
 int main(){
-	
+	int prices [] ={7,1,5,3,6,4};
+	printf("%d",maxProfit(prices,6));
 }
