a4ed98df698c07b02b00f3c287105cb98268a985
AdhamNour <adhamnourelwaffaa@gmail.com>
2024-05-18 22:22:42 +0300

    started solving the problem

---
+++ best-time-to-sell-and-buy-stocks/main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int maxProfit(int* prices, int pricesSize) {
+	int maximumProfit =0; // maximum profit calculated 
+	int smallestNumber = 10*10*10*10; // smallest number found in the array;  chosen to be thw maximum number possible for.the.input
+	int i=0; // loop index
+	for(i=0;i<pricesSize;i++){
+
+	}
+}
+
+
+int main(){
+	
+}
